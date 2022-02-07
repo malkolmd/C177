4b29226ded8bf6196b971532a5ddefd6b2c436ae
Malkolm Davis <31748297+malkolmd@users.noreply.github.com>
2022-02-06 18:50:15 -0600

    Add files via upload

---
+++ Guess-The-Number-REST-Service-master/Guess the Number REST Service/GuessTheNumber/src/main/java/com/sg/guessthenumber/dao/GameDao.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.guessthenumber.dao;
+
+import com.sg.guessthenumber.entity.Game;
+import java.util.List;
+
+public interface GameDao {
+    List<Game> getAllGames();
+    Game getGameById(int gameId);
+    Game addGame(Game game);
+    void updateGame(Game round);
+}
---
+++ Guess-The-Number-REST-Service-master/Guess the Number REST Service/GuessTheNumber/src/main/java/com/sg/guessthenumber/dao/RoundDao.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.guessthenumber.dao;
+
+import com.sg.guessthenumber.entity.Game;
+import com.sg.guessthenumber.entity.Round;
+import java.util.List;
+
+public interface RoundDao {
+    
+    List<Round> getAllRoundsByGameId(int gameId);
+    Round getRoundById(int roundId);
+    Round addRound(Round round);
+}
